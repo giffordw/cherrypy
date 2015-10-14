fcdbb5cfe3fa9cc5712d926ebe93fb11a99b10b6
giffordw <dgifford19@gmail.com>
2015-10-14 03:43:30 -0700

    initial commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from __future__ import (division, print_function, absolute_import)
+from .c4_richness import *
+from .c4_pairfind import *
+from DictEZ import *
+
+__version__ = "0.1"
